caed2d4c98c95f0fdbe1aa9e4bdeeef4a384f7e8
jhomn <jhomn@pop-os.localdomain>
2021-08-29 11:35:41 +0700

    add familira_function, multiple_arguments, string_methods

---
+++ 3function_and_packages/3string_methods.py
@@ -0,0 +1,12 @@
+# string to experiment with: place
+place = "poolhouse"
+
+# Use upper() on place: place_up
+place_up = place.upper() #capitalize()
+
+# Print out place and place_up
+print(place_up)
+print(place)
+
+# Print out the number of o's in place
+print(place.count('o'))
